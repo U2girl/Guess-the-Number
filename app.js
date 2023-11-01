d47d12eb1d4c4bddd188e7499169d843e1c403e4
U2girl <elycias@yahoo.com>
2023-11-01 18:27:19 -0400

    updated game choices

--- app.js
+++ app.js
@@ -5,37 +5,35 @@ User types in a number and presses Guess
 The User gets 5 chances and given hints
 */
 
-let userAgree = confirm("Welcome to Guess the Secret Number! Click ok to start guessing!")
-
+let userAgree = confirm("Welcome to Guess the Secret Number!  Click ok to start guessing")
 if(userAgree) {
-    
     let computerNumber = Math.ceil(Math.random()*10)
+    console.log(computerNumber)
     let counter = 0
     while(counter < 5) {
-         let userGuess = prompt ("Great! Please enter a number.")
-         if (userGuess == computerNumber) {
-            alert("Yay!!  You guessed the secret Number!!!")
+        let userGuess = prompt("Please enter a number between 1 and 20")
+        if(userGuess == computerNumber) {
+            alert("Yay! You guessed the Secret Number!!")
             break
         }
-      
-else {
-
-    alert("Sorry -- Try again!")
-
-}
-counter = counter + 1
-    }
+        else {
+            alert("Sorry! Wrong Number")
+        }
+        counter += 1
+          }
+    
 if(counter == 5) {
-alert("You lose! :(  Only 5 guesses allowed.")
+    alert("Sorry! Only 5 Guesses Allowed")
 }
-let playAgain = confirm("Would you like to play again?")
+let playAgain = confirm("Play again?")
 if(playAgain) {
     location.reload();
 }
 else {
-    alert("OK!  Come try again another time.")
+    alert("Ok! Come back and play another time.")
+
 }
 }
 else {
-    alert("Ok!  Come back to guess another time.")
+    alert("Ok! Come back and play another time.")
 }
